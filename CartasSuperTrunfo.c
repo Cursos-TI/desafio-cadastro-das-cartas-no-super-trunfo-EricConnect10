9cfb85588413563887a1d35942a1e89ed560493f
EricConnect10 <ericandrade22k@gmail.com>
2025-02-28 21:43:07 +0000

    Desafio de cartas.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,67 @@
 #include<stdio.h>
 int main(){
     printf("Desafio trunfo de cartas!\n");
+    printf("Carta 1 \n");
+
     char Estado = 'A';
-    char Código[20] = "A01";
+    char Código [20] = 'A01';
     char Nome[20] = "Minas gerais";
     int População = 213.22691;
     float Área = 586.528;
     float Pib = 272.3;
     int Pontos = 21;
     
-    printf("Estado: %s Estado\n");
-    scanf("%s", &Estado);
+    printf("Nome do Estado: \n");
+    scanf("%c", &Estado);
 
-    printf("Código da carta: \n");
-    scanf("%d", &Código);
+    printf("Qual é o Código do Estado: \n");
+    scanf("%s", &Código);
 
-    printf("nome da cidade: \n");
-    scanf("%s", &Nome);
+    printf("Nome da cidade: \n");
+    scanf(" %s", &Nome);
 
     printf("População: \n");
-    scanf("%f", &População);
+    scanf("%f" , &População);
 
-    printf("Área: %.2f km²\n");
-    scanf("%f", &Área);
+    printf("Tamanho da Área: \n");
+    scanf(" %d" , &Área);
+    
+    printf("Pib: \n ");
+    scanf(" %d" , &Pib);
 
-    printf("Pib: %.2f reais\n");
-    scanf("%f", &Pib);
+    printf("Número de pontos turisticos: \n");
+    scanf(" %c" , &Pontos);
 
-    printf("Número de Pontos turisticos: \n");
-    scanf("%d", &Pontos);
-  
+printf("Carta 2 \n");
+
+char ESTADO = 'B';
+char CÓDIGO[20] = "B01";
+char NOME[20] = "Goiás";
+int POPULAÇÃO = 7350.483;
+float ÁREA = 340.086;
+float PIB = 336.7;
+int PONTOS = 50;
+
+printf("Nome do Estado: \n");
+    scanf("%c", &ESTADO);
+
+    printf("Qual é o Código do Estado: \n");
+    scanf("%s", &CÓDIGO);
+
+    printf("Nome da cidade: \n");
+    scanf(" %s", &NOME);
+
+    printf("População: \n");
+    scanf("%f" , &POPULAÇÃO);
+
+    printf("Tamanho da Área: \n");
+    scanf(" %d" , &ÁREA);
+    
+    printf("Pib: \n ");
+    scanf(" %d" , &PIB);
+
+    printf("Número de pontos turisticos: \n");
+    scanf(" %c" , &PONTOS);
+
     return 0;
-
 }
